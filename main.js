1fa0f15681223bc78fd098446b336cfb694088e1
Alexander Kehr <alexanderkehr@outlook.com>
2022-12-21 14:05:42 +0100

    ViewManager um Regeln für alle Ansichten festzulegen

---
+++ main.js
@@ -0,0 +1,7 @@
+function goToStart(){
+    document.getElementById("categories").style.display = "none";
+    document.getElementById("prodView").style.display = "none";
+    document.getElementById("checkout").style.display = "none";
+    document.getElementById("m/f").style.display = "block";
+
+}
