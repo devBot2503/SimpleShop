8eff14a94caad5ff3d1f729dbe75b76c6c7baa37
Alexander Kehr <alexanderkehr@outlook.com>
2022-12-28 02:45:06 +0100

    Bases-Funktionalität

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function goToStart(){
-    document.getElementById("categories").style.display = "none";
-    document.getElementById("prodView").style.display = "none";
-    document.getElementById("checkout").style.display = "none";
-    document.getElementById("m/f").style.display = "block";
+/*
+Alexander Kehr
+Die Funktion lässt elemente verschwinden
+@parameter = targetId
+*/
+function hideElement(targetId){
+    document.getElementById(targetId).style.display = "none";
+}
 
+/*
+Alexander Kehr
+Die Funktion lässt elemente erscheinen
+@parameter = targetId
+*/
+function showElement(targetId){
+    document.getElementById(targetId).style.display = "block";
+}
+
+/*
+Alexander Kehr
+Die Funktion setzt den Page state, also die Ansicht innerhalb des viewManagers.
+@parameter = targetId
+*/
+function setPageState(targetId){ 
+    hideElement("categories");
+    hideElement("prodView");
+    hideElement("checkout");
+    hideElement("m|f");
+
+    showElement(targetId);
 }
